d18e043dc50989f52686f35df0a31c80a2a3728e
Vitaliiir16 <151745770+Vitaliiir16@users.noreply.github.com>
2024-02-26 16:04:39 +0000

    beer carousel first version

--- js/index.beer-carousel.js
+++ js/index.beer-carousel.js
@@ -1,42 +1,27 @@
 const slides = [
-    `<div class="carousel__slide">
-        <img height="150" width="150" src="imgs/imgs-video-beer-carousel/man-beer.png" alt="Slide 1">
-    </div>`,
-    `<div class="carousel__slide">
-        <img height="150" width="150" src="imgs/imgs-video-beer-carousel/two-glasses.png" alt="Slide 2">
-    </div>`,
-    `<div class="carousel__slide">
-        <img height="150" width="150" src="imgs/imgs-video-beer-carousel/orange-beer.png" alt="Slide 3">
-    </div>`,
-    `<div class="carousel__slide">
-        <img height="150" width="150" src="imgs/imgs-video-beer-carousel/beer-company.png" alt="Slide 4">
-    </div>`,
-    `<div class="carousel__slide">
-        <img height="150" width="150" src="imgs/imgs-video-beer-carousel/bar.png" alt="Slide 5">
-    </div>`,
+    `<img src="imgs/imgs-video-beer-carousel/man-beer.png" alt="Slide 1">`,
+    `<img src="imgs/imgs-video-beer-carousel/two-glasses.png" alt="Slide 2">`,
+    `<img src="imgs/imgs-video-beer-carousel/orange-beer.png" alt="Slide 3">`,
+    `<img src="imgs/imgs-video-beer-carousel/beer-company.png" alt="Slide 4">`,
+    `<img src="imgs/imgs-video-beer-carousel/bar.png" alt="Slide 5">`,
 ];
 
-let currentIdx = 0;
-function renderSlide() {
-    const slidContainer = document.querySelector('.beer-carousel__slide');
-    slidContainer.innerHTML = slides[currentIdx];
-};
+function renderSlides() {
+    const slideContainer = document.querySelector('.beer-carousel__slide');
+    slideContainer.innerHTML = slides.map(slide => `<div class="carousel__slide">${slide}</div>`).join('');
+}
 
-renderSlide();
-
-function nextSlide() {
-    currentIdx = currentIdx + 1 >= slides.length ? 0 : currentIdx + 1;
-    renderSlide();
-};
-
-function prevSlide() {
-    currentIdx = currentIdx - 1 < 0  ? slides.length -1: currentIdx - 1;
-    renderSlide();
-};
-
+renderSlides();
 
 const btnNext = document.querySelector('.carousel-button__button-next');
-btnNext.addEventListener('click', nextSlide);
-
 const btnPrev = document.querySelector('.carousel-button__button-prev');
-btnPrev.addEventListener('click', prevSlide);
+
+btnNext.addEventListener('click', () => {
+    const slideContainer = document.querySelector('.beer-carousel__slide');
+    slideContainer.appendChild(slideContainer.firstElementChild);
+});
+
+btnPrev.addEventListener('click', () => {
+    const slideContainer = document.querySelector('.beer-carousel__slide');
+    slideContainer.insertBefore(slideContainer.lastElementChild, slideContainer.firstElementChild);
+});
