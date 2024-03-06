4b1332345932e60e94f55113600142b7897b997f
Tamila57 <156967484+Tamila57@users.noreply.github.com>
2024-03-06 14:31:10 +0000

    add js to send form by email, add top img and css for it

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ document.addEventListener("partialsLoaded", () => {
     import("./history-slider-beer.js");
     import("./faq-beer.js");
     import("./index.beer-carousel.js");
+    import("./footer.js");
 });
 
 
